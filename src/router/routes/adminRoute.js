e560eb4f5c577aba2be9f5ff229e18917d4d54af
coredeveloper.2023@gmail.com <coredeveloper.2013@gmail.com>
2023-07-18 00:32:14 +0600

    seller page added

--- src/router/routes/adminRoute.js
+++ src/router/routes/adminRoute.js
@@ -2,6 +2,7 @@ import {lazy} from "react";
 const AdminDashboard =lazy(()=>import("../../views/admin/adminDashboard"))
 const Orders =lazy(()=>import("../../views/admin/Orders"))
 const Category =lazy(()=>import("../../views/admin/Category"))
+const Sellers =lazy(()=>import("../../views/admin/Sellers"))
 
 export const adminRoutes=[
     {
@@ -16,6 +17,10 @@ export const adminRoutes=[
     {
         path:'admin/dashboard/category',
         element:<Category/>,
+        role:'admin'
+    },  {
+        path:'admin/dashboard/sellers',
+        element:<Sellers/>,
         role:'admin'
     },
 ]
--- src/views/admin/Sellers.jsx
+++ src/views/admin/Sellers.jsx
@@ -1,24 +1,20 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
-import {FaEdit} from "react-icons/fa";
+import {FaEdit, FaEye} from "react-icons/fa";
 import {AiFillDelete, AiOutlineArrowDown, AiOutlineArrowLeft} from "react-icons/ai";
 import Pagination from "../Pagination";
 import {BsImage} from "react-icons/bs";
 
-const Category = () => {
+const Sellers = () => {
     const [currentPage, setCurrentPage] = useState(1)
     const [searchValue, setSearchValue] = useState('')
     const [status, setStatus]           = useState(true)
     const [perPage, setPerPage]         = useState(5)
     const [show,setShow]=useState(false)
     return (<div className='px-2 lg:px-7 pt-5'>
-        <div className='flex lg:hidden justify-between items-center mb-5 px-4 bg-slate-700 rounded-md py-2'>
-       <h1 className='text-blue-300 font-semibold text-center'>Categorys</h1>
-            <button onClick={()=>setShow(!show)}  className='bg-indigo-500 hover:bg-indigo-400 px-4 py-3 text-xl rounded-md'>Add </button>
 
-        </div>
         <div className='flex flex-wrap w-full'>
-            <div className='w-full lg:w-7/12'>
+            <div className='w-full '>
                 <div className='w-full p-4 bg-slate-800 rounded-md'>
                     <div className='flex justify-between items-center'>
                         <select onChange={(e) => setPerPage(parseInt(e.target.value))}
@@ -38,6 +34,11 @@ const Category = () => {
                                 <th scope='col' className='py-3 px-4'>No</th>
                                 <th scope='col' className='py-3 px-4'>Img</th>
                                 <th scope='col' className='py-3 px-4'>Name</th>
+                                <th scope='col' className='py-3 px-4'>Shop Name</th>
+                                <th scope='col' className='py-3 px-4'>Payment Status</th>
+                                <th scope='col' className='py-3 px-4'>Email</th>
+                                <th scope='col' className='py-3 px-4'>Devision</th>
+                                <th scope='col' className='py-3 px-4'>District</th>
                                 <th scope='col' className='py-3 px-4'>Action</th>
                             </tr>
                             </thead>
@@ -46,21 +47,13 @@ const Category = () => {
                                 <td scope='col' className='py-3 px-4 font-medium whitespace-nowrap'>3</td>
                                 <td scope='col' className='py-3 px-4 font-medium whitespace-nowrap'>3</td>
                                 <td scope='col' className='py-3 px-4 font-medium whitespace-nowrap'>3</td>
+                                <td scope='col' className='py-3 px-4 font-medium whitespace-nowrap'>3</td>
+                                <td scope='col' className='py-3 px-4 font-medium whitespace-nowrap'>3</td>
+                                <td scope='col' className='py-3 px-4 font-medium whitespace-nowrap'>3</td>
+                                <td scope='col' className='py-3 px-4 font-medium whitespace-nowrap'>3</td>
+                                <td scope='col' className='py-3 px-4 font-medium whitespace-nowrap'>3</td>
                                 <td scope='col' className='py-3 px-4 font-medium whitespace-nowrap'>
-                                    <div className='flex items-center gap-2'>
-                                        <Link>
-                                            <div
-                                                className='bg-indigo-900 rounded-lg h-[40px] w-[40px] flex items-center justify-center'>
-                                                <FaEdit/>
-                                            </div>
-                                        </Link>
-                                        <Link>
-                                            <div
-                                                className='bg-red-600 rounded-lg h-[40px] w-[40px] flex items-center justify-center'>
-                                                <AiFillDelete/>
-                                            </div>
-                                        </Link>
-                                    </div>
+                                 <Link ><FaEye/></Link>
                                 </td>
                             </tr>
                             </tbody>
@@ -77,35 +70,9 @@ const Category = () => {
                     </div>
                 </div>
             </div>
-            <div className={`w-[320px] lg:w-5/12 translate-x-100 lg:relative lg:right-0 fixed ${show ? 'right-0':'-right-[340px]'} z-[9999] top-0 transition-all duration-500`}>
-                <div className='w-full pl-5'>
-                <div className=' bg-slate-800  h-screen lg:h-auto px-3 py-2 lg:rounded-md'>
-                    <div onClick={()=>setShow(!show)} className='cursor-pointer block lg:hidden flex items-center justify-center bg-slate-800 h-[50px] w-[50px] bg-sky-300 rounded-md'>
-                    <AiOutlineArrowLeft  />
-                    </div>
-                      <h1 className='text-white font-semibold text-xl w-full text-center'>Add Category</h1>
-                    <div className='flex flex-col w-full gap-1 mb-3'>
-                        <label htmlFor="name" className='text-white'>Category name</label>
-                        <input type="text" className='px-4 py-2 focus:border-indigo-400 outline-none bg-slate-800 border border-slate-700 rounded-md text-white ' name={'category_name'} id='name' placeholder='category name'/>
-                    </div>
-                    <div>
-                        <label htmlFor="img" className='flex justify-center items-center flex-col h-[238px] cursor-pointer border border-dashed hover:border-indigo-500'>
-                            <span><BsImage/></span>
-                            <span>select Image</span>
-                        </label>
-                    </div>
-                    <input type="file" className='hidden' name='image' id='image'/>
-                    <div>
-                        <button className='bg-black text-white border rounded-md w-full px-7 py-3 text-xl mt-5 hover:bg-slate-800'>
-                            Add Category
-                        </button>
-                    </div>
-                </div>
 
-                </div>
-            </div>
         </div>
     </div>);
 };
 
-export default Category;
+export default Sellers;
